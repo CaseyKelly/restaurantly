7efa5665e4786b9cfdd543c8c9404fefa3b49de8
CaseyKelly <casey.kelly@colorado.edu>
2015-02-28 12:39:58 -0700

    Clean up rspecs

--- spec/requests/restaurant_spec.rb
+++ spec/requests/restaurant_spec.rb
@@ -7,13 +7,8 @@ describe RestaurantsController do
     end
 
     context "resource exists" do
-      subject {get "restaurants/#{@restaurant.id}"}
+      subject {get "/restaurants/#{@restaurant.id}"}
       it { expect(subject).to render_template(:show)}
-    end
-
-    context "resource doesn't exist" do
-      subject {get "restaurants/#{@restaurant.id + 1}"}
-      it { expect(subject).to redirect_to(:root) }
     end
   end
 end
